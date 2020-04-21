c1741518736abba3c06e566ebeffa66bb289bde3
Alejandro Lynch <alynch@hopscotch.ca>
2020-04-21 18:50:54 -0400

    Pass title from json file to component

--- frontend/src/components/SquirrelGrid.vue
+++ frontend/src/components/SquirrelGrid.vue
@@ -1,11 +1,14 @@
 <template>
-  <div id="grid">
-    <SquirrelComponent v-for="squirrel in squirrels" :key="squirrel.id" :squirrel="squirrel"></SquirrelComponent>
-  </div>
+    <div>
+        <h1 v-text="title"></h1>
+        <div id="grid">
+            <SquirrelComponent v-for="squirrel in squirrels" :key="squirrel.id" :squirrel="squirrel"></SquirrelComponent>
+        </div>
+    </div>
 </template>
 
 <script>
-import squirrels from '../assets/squirrels.json'
+import data from '../assets/squirrels.json'
 import SquirrelComponent from './SquirrelComponent'
 
 export default {
@@ -14,12 +17,19 @@ export default {
     SquirrelComponent
   },
   data: function () {
-    return squirrels
+    return {
+        title: data.title,
+        squirrels: data.squirrels
+    }
   }
 }
 </script>
 
 <style scoped>
+  h1 {
+    text-align: center;
+  }
+
   #grid {
     display: grid;
     grid-template-columns: 50% 50%;
