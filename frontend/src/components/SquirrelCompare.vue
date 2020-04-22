8fb44cf266cab37cefa7913dbfc2cac0490668c4
Alejandro Lynch <alynch@hopscotch.ca>
2020-04-22 08:22:07 -0400

    Create filter function
    Ignores one-letter terms
    Case-sensitive
    Matches starting from beggining of word
    Filters on name and color

---
+++ frontend/src/components/SquirrelCompare.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h1 v-text="title"></h1>
+        <div id="grid">
+            <SquirrelComponent v-for="squirrel in squirrels" :key="squirrel.id" :squirrel="squirrel"></SquirrelComponent>
+        </div>
+    </div>
+</template>
+
+<script>
+import data from '../assets/squirrels.json'
+import SquirrelComponent from './SquirrelComponent'
+
+export default {
+  name: 'SquirrelGrid',
+  components: {
+    SquirrelComponent
+  },
+  data: function () {
+    return {
+        title: data.title,
+        squirrels: data.squirrels
+    }
+  }
+}
+</script>
+
+<style scoped>
+  h1 {
+    text-align: center;
+  }
+
+  #grid {
+    display: grid;
+    grid-template-columns: 50% 50%;
+  }
+</style>
--- frontend/src/components/SquirrelGrid.vue
+++ frontend/src/components/SquirrelGrid.vue
@@ -1,25 +1,45 @@
 <template>
     <div>
         <h1 v-text="title"></h1>
+
+        <label>
+            Filter:
+            <input type="text" v-model="filter"/>
+        </label>
+
         <div id="grid">
-            <SquirrelComponent v-for="squirrel in squirrels" :key="squirrel.id" :squirrel="squirrel"></SquirrelComponent>
+            <figure v-for="squirrel in filteredSquirrels" :key="squirrel.id">
+                <img :src="'./images/' + squirrel.images[0].src" :alt="''">
+            </figure>
         </div>
     </div>
 </template>
 
 <script>
 import data from '../assets/squirrels.json'
-import SquirrelComponent from './SquirrelComponent'
 
 export default {
   name: 'SquirrelGrid',
-  components: {
-    SquirrelComponent
-  },
   data: function () {
     return {
         title: data.title,
-        squirrels: data.squirrels
+        squirrels: data.squirrels,
+        filter: ''
+    }
+  },
+
+  computed: {
+    filteredSquirrels: function () {
+      if (!this.filter || this.filter.length < 2) {
+        return this.squirrels;
+      }
+
+      /*return this.squirrels.filter(item => 
+        item.color.localeCompare(this.filter, 'en', {sensitivity: 'base'}) === 0
+            || item.name.localeCompare(this.filter, 'en', {sensitivity: 'base'}) === 0
+      )*/
+      return this.squirrels.filter(item => 
+        item.color.startsWith(this.filter) || item.name.startsWith(this.filter))
     }
   }
 }
@@ -34,4 +54,9 @@ export default {
     display: grid;
     grid-template-columns: 50% 50%;
   }
+
+  img {
+    width: 150px;
+    height: 150px;
+   } 
 </style>
